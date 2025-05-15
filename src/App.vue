08ee6360b39141c103770581d401a35a9cd9b0b4
daixingmei <daixingmei@sunlands.com>
2025-05-15 23:19:25 +0800

    submit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        
+          <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
@@ -10,7 +11,7 @@ export default {
   name: 'App'
   ,
   components: 
-  {
+           {
     HelloWorld
   }
 }
