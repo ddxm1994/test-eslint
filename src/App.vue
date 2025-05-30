205e6b39826b835f0893ac9a23fe218d4f63b998
daixingmei <daixingmei@sunlands.com>
2025-05-30 10:01:42 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
@@ -18,7 +17,6 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
